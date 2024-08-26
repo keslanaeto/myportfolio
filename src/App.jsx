516865513270e69c1a72e8047a6170ac448afeda
kesla chime <keslachime.n@gmail.com>
2024-08-26 22:24:06 +0100

    ch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
-// import Nav from './components/Navbar'
-// import Naviga from './components/Naviga'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import About from './components/About'
 import Skills from './components/Skills'
 import Qualification from './components/Qualification'
 import Projects from './components/Projects'
-import Footer from './components/Footer'
+
 
 
 
@@ -21,7 +19,6 @@ const App = () => {
        <Skills/>
        <Qualification/>
        <Projects/>
-       <Footer />
        
     </div>
     
