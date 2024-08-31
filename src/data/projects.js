b72e9945dc4c184a5752e590b529665f515c3513
kesla chime <keslachime.n@gmail.com>
2024-08-31 13:19:48 +0100

    change

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,27 +1,29 @@
 export const projects  = [
 
+
+    {
+        title: "Brown furnitures",
+        imageSrc: "assets/cover.jpg",
+        Demo:  "https://brown-furnitures.vercel.app/",
+        Code: " https://github.com/keslanaeto/Brown-furnitures",
+        skills: ["HTML", " CSS",  "Javasccript"],
+       
+    },
+
+
 {
     title: "Foodolis",
-    imageSrc: "assets/pancake.png",
+    imageSrc: "assets/foodcover.jpg",
     Demo: "https://foodolis.vercel.app/",
     Code: " https://github.com/keslanaeto/myfoods",
     skills: ["HTML", "TailwindCss", "React.JS"],
    
 }, 
 
-{
-    title: "Brown furnitures",
-    imageSrc: "assets/cover.jpg",
-    Demo:  "https://brown-furnitures.vercel.app/",
-    Code: " https://github.com/keslanaeto/Brown-furnitures",
-    skills: ["HTML", " CSS",  "Javasccript"],
-   
-},
-
 
 {
     title: "Ellatrends",
-    imageSrc: "assets/ella.png",
+    imageSrc: "assets/dresscover.png",
     Demo: "https://ellatrends.vercel.app/",
     Code: "https://github.com/keslanaeto/trends-project",
     skills: ["HTML", "TailwindCss", "React.JS"],
